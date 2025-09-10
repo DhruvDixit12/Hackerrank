81e2cb14dffbbe652a478ddf880da077240e8a71
DhruvDixit12 <143922932+DhruvDixit12@users.noreply.github.com>
2025-09-10 16:41:44 +0530

    Create Sum vs XOR.cpp

---
+++ Sum vs XOR.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n; cin >> n;
+    if (n == 0) { cout << 1; return 0; }
+    int zeros = 0;
+    while (n) {
+        if ((n & 1) == 0) zeros++;
+        n >>= 1;
+    }
+    cout << (1LL << zeros);
+    return 0;
+}
